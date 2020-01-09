30c5016f986eea4e7e470ef4a4fbc7ce5fe88736
sheetalc818 <sheetalc818@gmail.com>
2020-01-09 16:31:42 +0530

    Added : Flipping coin multiple time and calculating percentage

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,18 +1,53 @@
 #!/bin/bash -x
 echo "===FlipCoin Simulation==="
 
-#UC1-Flipping a Coin to Display Heads or Tails 
+#Variable
+flipCount=0;
+counter=0;
+countHeads=0;
+countTails=0;
+percentHead=0;
+percentTail=0;
+
+#Dictionary
+declare -A Combination
+
+#Taking input from user
+read -p "Enter How many times You want to flip Coin :" flipCount
+
+#Calculating Percentage for Head and Tail
+function calPercentage()
+{
+	percentHead=$((($countHeads*100)/$flipCount))
+	echo "PercentHead:" $percentHead
+
+	percentTail=$((($countTails*100)/$flipCount))
+	echo "PercentTails: " $percentTail
+}
 
 #((RANDOM)) to find Heads or Tails
 function getFlip()
 {
-	if [ $((RANDOM%2)) -eq 1 ]
-	then
-		echo "HEAD"
-	else
-		echo "TAIL"
-	fi
+	local count=$1
+	while [ $count -ne 0 ]
+	do
+		if [ $((RANDOM%2)) -eq 1 ]
+		then
+			result="HEAD"
+			Combination[$((counter++))]=$result
+			((countHeads++))
+		else
+			result="TAIL"
+			combination[$((counter++))]=$result
+			((countTails++))
+		fi
+		((count--))
+	done
+	echo $countHeads $countTails
+	
+	#Calling calPercentage function
+	calPercentage $countHeads $countTails $flipCount
 }
 
 #Calling getFlip function
-getFlip
+getFlip $flipCount
